4651d44e06e1595c16ededc2b020d024beef4153
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-06-20 20:41:11 +0530

    ninth

--- Warning.ps1
+++ Warning.ps1
@@ -5,26 +5,3 @@ if($myAge -le 17){
 else{
     write-host "Welcome to the club"
 }
-#trying out special expressions
-
-<#"this is my string" -cmatch "str"
-
-"this is my string" -cnotmatch "str"
-
-"this is my string1" -cmatch "\d"
-
-"this is my string" -replace "string","dog"#>
-
-<#$var= read-host "enter your password"
-$var2= read-host "confirm your password"
-
-if($var -eq $var2){
-    write-host "password accepted"
-}
-else{
-    write-host "password not accepted"
-}
-
-$var3= read-host "enter new password"
-#enter break here till if=else
-$var -replace $var3#>
---
+++ ranges.ps1
@@ -0,0 +1,5 @@
+#we work with ranges
+
+"Now I check this string" -cmatch "(I|u)" #checking if either I or u exists in string]
+"This is my string" -cmatch "[a-zA-Z]"
+
