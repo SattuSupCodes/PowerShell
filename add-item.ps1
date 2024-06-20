81630a4362f3a04887f1827522eb36c35f9540ee
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-06-20 17:04:19 +0530

    third

--- add-item.ps1
+++ add-item.ps1
@@ -1,4 +1,6 @@
 <#New-Item -path "C:\users\HP\Desktop\PowerShell" -name "trynew.ps1" -ItemType File
 Rename-Item -path "C:\users\HP\Desktop\PowerShell\trynew.ps1" -NewName "forEach.ps1"#>
 
-New-Item -path "C:\users\HP\Desktop\PowerShell" -name "Warning.ps1" -ItemType File 
+#New-Item -path "C:\users\HP\Desktop\PowerShell" -name "Warning.ps1" -ItemType File 
+
+New-Item -path "C:\users\HP\Desktop\blogsite" -name "siteAssets"-ItemType Directory
