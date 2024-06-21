1f9673e0215a5143e79076e1580abc0da2c68cfe
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-06-21 20:05:52 +0530

    haha

--- ranges.ps1
+++ ranges.ps1
@@ -3,4 +3,4 @@
 "Now I check this string" -cmatch "(I|u)" #checking if either I or u exists in string]
 "This is my string" -cmatch "[a-zA-Z]" #checking if string characters in range of a-z and A-Z
 "123456" -cmatch "[0-4]" #checking if numbers lie in defined range
-"1234" -cmatch "[6-9]"
+"1234" -cmatch "[6-9]" #returns false 
