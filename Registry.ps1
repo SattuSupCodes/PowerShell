c5e22a1766c0d330d6dc42ab0437c6949ebba32b
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-06-22 17:47:47 +0530

    registry

--- Registry.ps1
+++ Registry.ps1
@@ -8,6 +8,17 @@ HKCU: HKEY_CURRENT_USER  contains configuration information of windows relevant 
 (specifically the current user)
 for eg: Installed printers, desktop wallpaper etc.
 
-HKU: HKEY_USERS 
+HKU: HKEY_USERS contains user-specific config. informations for all active users on the computer. 
+This hive corresponds to a user on the system and is named with that user's security identifier, or SID.
+
+HKCR: HKEY_CLASSES_ROOT contains files name extension associations and COM class registration
+such as progIDS, CLSIDs. This registry hive contains the necessary information for Windows to know what 
+to do when you ask it to do something, like to view the contents of a drive, 
+or open a certain type of file, etc. 
+
+
+HKCC: HKEY_CURRENT_CONFIG is a registry hive that's a part of the Windows Registry. 
+It doesn't store any information itself but instead acts as a pointer, or a shortcut, 
+to a registry key that keeps the information about the hardware profile currently being used.
 #>
 
