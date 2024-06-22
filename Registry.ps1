0d6fe7e6e95c718762e5ebecbe809de23609c88e
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-06-22 17:47:15 +0530

    registry

---
+++ Registry.ps1
@@ -0,0 +1,13 @@
+<#managing files, folders, and registry items#>
+
+<#Basic about several registries:
+HKLM: HKEY_LOCAL_MACHINE makes up the windows registry. This hive contains majority of the 
+configuration informations of the windows software installed, as for the OS itself.
+
+HKCU: HKEY_CURRENT_USER  contains configuration information of windows relevant of the user logged in.
+(specifically the current user)
+for eg: Installed printers, desktop wallpaper etc.
+
+HKU: HKEY_USERS 
+#>
+
