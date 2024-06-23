8301ef37a7354cf8938fa1e54cc51bfc9766e42f
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-06-23 16:44:59 +0530

    WMI

--- windows_management/WMIstructure.ps1
+++ windows_management/WMIstructure.ps1
@@ -1,4 +1,4 @@
-<#WMI is made up of three components. These three components include the WMI
+<# WMI is made up of three components. These three components include the WMI
 consumers, the WMI infrastructure, and the WMI providers. When you are using
 PowerShell, you will leverage all three of these components to interact with the
 hardware and operating system.
@@ -9,3 +9,10 @@ interact with the hardware or the operating system. This can include fields that
 be read, or actions that can be invoked on the hardware itself.
 
 #>
+
+get-wmiobject -namespace root\cimv2 -class win32_computersystem
+# using cimsession to call instead of wmiobject
+get-ciminstance -namespace root\cimv2 -class win32_computersystem
+<#The get-ciminstance cmdlet has the ability to run on
+remote systems over a CIM session, whereas the get-wmiobject only permits local
+execution.#>
