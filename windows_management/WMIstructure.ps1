a1a4afeeeac05c68b022513690c2f4888538f358
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-06-23 16:34:58 +0530

    WMI

---
+++ windows_management/WMIstructure.ps1
@@ -0,0 +1,11 @@
+<#WMI is made up of three components. These three components include the WMI
+consumers, the WMI infrastructure, and the WMI providers. When you are using
+PowerShell, you will leverage all three of these components to interact with the
+hardware and operating system.
+
+The MOF files provide the
+WMI infrastructure with a set of instructions on how the WMI consumers may
+interact with the hardware or the operating system. This can include fields that can
+be read, or actions that can be invoked on the hardware itself.
+
+#>
