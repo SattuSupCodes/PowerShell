fb7f4e82e84ee41fc95df8b531351dfbe1834e17
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-06-30 19:39:07 +0530

    upd

---
+++ XML/XML.ps1
@@ -0,0 +1,12 @@
+<#XML manipulation
+
+When you are working with Microsoft-based systems, there is a high probability
+that you are leveraging eXtensible Markup Language (XML) for data and
+communications. XML was created by the World Wide Web Consortium (W3C)
+to standardize the encoding of documents to make them both legible to humans
+and usable by computer systems. XML's format is very similar to that of Hypertext
+Markup Language (HTML). If you know the basics of HTML, you should be able
+to pick up XML pretty quickly. While the syntax is very similar between HTML
+and XML, the purposes of these languages are very different. HTML is used by web
+browsers to render objects and text on a website. XML is used to encapsulate data to
+be stored on a system, or passed between systems.#>
