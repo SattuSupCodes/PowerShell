46ba669c9b26d9b7221ebf45c512323cfe7cec4c
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-06-30 19:32:52 +0530

    upd

--- windows_management/searchingWMI_Classes.ps1
+++ windows_management/searchingWMI_Classes.ps1
@@ -1,3 +1,5 @@
 get-wmiobject -list | Where-Object{$_.Name -like "*Time*"}
 $classProperties = (get-cimclass -class win32_Printer).CimClassProperties
 $classProperties.count
+
+get-wmiobject -list | where{$_.Name -like "*time*"}
